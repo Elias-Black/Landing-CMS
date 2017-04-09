96dcb7ebd8498d4cac36ba23f52c02442ab979d2
elias <iliachernykh@yahoo.com>
2017-04-09 23:20:18 +0500

    Now all Field Types diefined as a constant

--- cms/_classes/index.php
+++ cms/_classes/index.php
@@ -23,12 +23,29 @@ define('RANDOM_STR_LENGTH',	-10);
 
 define('AUTH_EXPIRE',	31536000);
 
+
+
 require_once("utils.class.php");
 
 require_once("content.class.php");
 
 require_once("user.class.php");
 
+
+
 $message = User::auth();
+
+
+
+$field_types = array(
+	'String'			=>	'input_text',
+	'HTML (WYSIWYG)'	=>	'textarea_html',
+	'Multiline text'	=>	'textarea_text',
+	'Checkbox'			=>	'input_checkbox',
+	'Color Picker'		=>	'input_color_picker',
+	'File Uploader'		=>	'input_file_uploader',
+);
+
+define('FIELD_TYPES',	serialize($field_types));
 
 ?>

652957937455513f4a3d6ed07c506e26c8dbdddd
elias <iliachernykh@yahoo.com>
2017-08-24 14:45:40 +0500

    Fix for redirecting from Login Page for authorized.

--- cms/login/index.php
+++ cms/login/index.php
@@ -3,8 +3,13 @@
 // Connecting main classes
 require_once('../_classes/index.php');
 
+// Logout
 if( isset($_GET['logout']) )
 	User::logout();
+
+// Redirect to CMS if logged in
+if( defined('IS_LOGIN') )
+	Utils::redirect( Utils::getLink('cms/') );
 
 // Render login form
 $content = Utils::render(

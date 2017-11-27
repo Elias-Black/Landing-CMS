00347ed4e6d8b85b873a730d2329977e57ffe4a1
elias <iliachernykh@yahoo.com>
2017-11-28 00:18:39 +0500

    Fix for termination PHP when DB is not writable or readable.

--- cms/_classes/db.class.php
+++ cms/_classes/db.class.php
@@ -38,13 +38,20 @@ class DB
 
 		$result = array();
 
+
 		$private_db_path = Utils::getPath(self::PRIVATE_DB_PATH);
 
 		try
 		{
+
+			if( is_readable($private_db_path) !== TRUE )
+			{
+				throw new Exception();
+			}
+
 			$content = file_get_contents($private_db_path);
 
-			if( $content === false && file_exists($private_db_path) )
+			if($content === FALSE)
 			{
 				throw new Exception();
 			}
@@ -69,6 +76,7 @@ class DB
 
 		$result['error'] = false;
 		$result['db_content'] = $safe_replace ? Utils::replaceQuotesInArray($content) : $content;
+
 
 		return $result;
 
@@ -108,13 +116,20 @@ class DB
 
 		$result = array();
 
+
 		$password_db_path = Utils::getPath(self::PASSWORD_DB_PATH);
 
 		try
 		{
+
+			if( is_readable($password_db_path) !== TRUE )
+			{
+				throw new Exception();
+			}
+
 			$content = file_get_contents($password_db_path);
 
-			if( $content === false && file_exists($password_db_path) )
+			if($content === false)
 			{
 				throw new Exception();
 			}
@@ -133,6 +148,7 @@ class DB
 		$result['error'] = false;
 		$result['db_password'] = substr($content, self::SECURE_LENGTH);
 
+
 		return $result;
 
 	}
@@ -145,9 +161,17 @@ class DB
 
 		$content = self::SECURE_TEXT . $password;
 
+		$password_db_path = Utils::getPath(self::PASSWORD_DB_PATH);
+
 		try
 		{
-			$updated = file_put_contents( Utils::getPath(self::PASSWORD_DB_PATH), $content );
+
+			if( is_writable($password_db_path) !== TRUE )
+			{
+				throw new Exception();
+			}
+
+			$updated = file_put_contents($password_db_path, $content);
 
 			if($updated === false)
 			{
@@ -162,6 +186,7 @@ class DB
 			$result['error_message'] = 'Can not be written to the Password database. Check permissions on <a href="'.Utils::getLink('install.php').'" target="_blank">this</a> helper.';
 
 		}
+
 
 		return $result;
 
@@ -179,9 +204,17 @@ class DB
 
 		$content = self::SECURE_TEXT . serialize($content);
 
+		$private_db_path = Utils::getPath(self::PRIVATE_DB_PATH);
+
 		try
 		{
-			$updated = file_put_contents( Utils::getPath(self::PRIVATE_DB_PATH), $content );
+
+			if( is_writable($private_db_path) !== TRUE )
+			{
+				throw new Exception();
+			}
+
+			$updated = file_put_contents($private_db_path, $content);
 
 			if($updated === false)
 			{
@@ -209,9 +242,17 @@ class DB
 
 		$content = self::getFieldsOutput($content);
 
+		$public_db_path = Utils::getPath(self::PUBLIC_DB_PATH);
+
 		try
 		{
-			$updated = file_put_contents( Utils::getPath(self::PUBLIC_DB_PATH), serialize($content) );
+
+			if( is_writable($public_db_path) !== TRUE )
+			{
+				throw new Exception();
+			}
+
+			$updated = file_put_contents($public_db_path, serialize($content) );
 
 			if($updated === false)
 			{
